4f251a57cce4d3763003873368d934b084df4ed1
shuyan0723 <2737559680@qq.com>
2025-06-03 21:29:28 +0800

    vue+react

--- algorithm/4.js
+++ algorithm/4.js
@@ -2,6 +2,9 @@ const bigNum=123456789012345678901234567890123456789n
 // BigInt 申明方式 函数申明
 // bigint 简单数字类型，不是对象，不是构造函数，不能new
 const theNum= BigInt("123456789012345678901234567890123456789")
+const theNum1= BigInt("123456789012345678901234567890123456789")
+console.log(theNum+theNum1);
+
 console.log(bigNum,theNum,typeof bigNum,typeof theNum,typeof 1);
 console.log(bigNum+1n);
 //   BigInt不能用new
--- js/type/2.js
+++ js/type/2.js
@@ -3,6 +3,6 @@ console.log(0/0);
 console.log(Math.sqrt(-1));// js 内置的Math对象，提供了一些数学运算的方法，如sqrt（）
 console.log(parseInt('123'),parseInt('a456'),parseInt('456a'));
 console.log(Number(undefined));// NaN
-console.log(NaN===NaN);// false Not a Number 不是数字的方式有很多种
+console.log(NaN===NaN);// false Not a Number 不是数字的方式有很多种 NaN是一个特殊值
 console.log(isNaN(NaN),isNaN(0/0),isNaN('456a'),(typeof('456a') !=='number'));// true 是数字的方式有很多种
 console.log(typeof NaN);
