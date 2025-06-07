c7e1bf28b0d3d1a26b0a6cc93fff3a9df9db4db9
shuyan0723 <2737559680@qq.com>
2025-06-07 17:08:28 +0800

    react4

--- react/vite-project/src/components/TodoForm.jsx
+++ react/vite-project/src/components/TodoForm.jsx
@@ -1,9 +1,31 @@
-function TodoForm() {
+import { useState } from 'react'
+function TodoForm(props) {
+    const onAdd=props.onAdd;
+    const [text,setText]=useState('打吃鸡')
+
+    const handleSubmit=(e)=>{
+        //阻止默认提交行为
+        //由js 来控制
+        e.preventDefault();//event api
+        // console.log(text);
+        onAdd(text)
+        //todos? 打报告 
+    }
+    const hangleChange=(e)=>{
+        // console.log(e.target.value);
+        setText(e.target.value)
+
+    }
   return (
    
-      <form>
-        <input type="text" />
-        <button>Add</button>
+    <form action ="http://www.baidu.com" onSubmit={handleSubmit}>
+    <input
+     type="text"
+     placeholder="请输入代办事项"
+     value={text}
+     onChange={hangleChange}
+     />
+     <button>提交</button>
       </form>
   )
 }
--- react/vite-project/src/components/TodoList.jsx
+++ react/vite-project/src/components/TodoList.jsx
@@ -1,12 +1,74 @@
+// 内置的hook 函数 
+import { useState } from 'react'
 import '../todo.css'
 import TodoForm from './TodoForm'
 import Todos from './Todos'
 function TodoList(){
+    //数据驱动的界面
+    //静态页面？
+    //DOM 数组 -> map -》 join('') -> innerHTML 底层API 编程
+    //缺点 低效，面向API
+    //面向业务 懂业务
+    //数据  -> 变化 ->数据状态 -> 自动刷新页面 -> **数据****驱动**界面
+    //数组，第一项是数据变量，第二项函数 执行，并传入新的todos
+    //页面会自动更新
+    //挂载点 tbody
+    //{todos.map}
+    // setTodos  DOM 及动态更新
+    // 响应式界面开发 
+    // hi 数据状态 setHi 修改数据状态的方法
+    // es6 解构赋值
+    // const h1=useState('haha')[0];
+    const[h1,setHi]=useState('haha');
+    const [title,SetTitle] = useState('Todo list')
+    const [todos,setTodos]=  useState([
+        {
+            id:1,
+            text:'吃饭',
+            completed:false
+        }
+    ])
+
+
+    const handleAdd=(text)=>{
+        setTodos([
+            ...todos,{
+                id:todos.length+1,
+                text:text,
+                completed:false
+            }
+        ])
+    }
+    // setTimeout(()=>{
+    //     setTodos([...todos,
+    //         {
+    //          id:2,
+    //          text:'睡觉',
+    //          completed:false
+    //         }
+    //     ])
+    //     // 找到DOM,innerHTML
+    //     // 更新业务 setTitle 
+    //     SetTitle('Todo list 2')
+    //     setHi('奥力给')
+    // },2000)
     return(
    <div className="container">
-    <h1 className="title">Todo List</h1>
-    <TodoForm />
-    <Todos />
+    <h1 className="title">{title }{h1}</h1>
+    {/* 表单 */}
+    <TodoForm onAdd={handleAdd}/>
+    {/* 列表 */}
+    <Todos todos={todos} />
+    {/* {
+        //当下这个位置
+        //数据为王 界面是被驱动的
+        //数据驱动
+        //数据绑定 data binding
+        //发生改变后 自动的改变
+        todos.map(todo=>(
+            <li>{todo.text}</li>
+        ))
+    } */}
    </div>
     )
 }
