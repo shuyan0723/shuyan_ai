6e7b685da28e7221308fb4939a988e65d7548d35
shuyan0723 <2737559680@qq.com>
2025-07-10 21:22:58 +0800

    react-useState

---
+++ react/context-demo/src/hooks/useTheme.js
@@ -0,0 +1,7 @@
+// 自定义hooks 
+// hooks + component render
+import { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
+export function useTheme(){
+    return useContext(ThemeContext);
+}
