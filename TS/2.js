eb701a8315381a724e1ecaf13f3a8ea9ee7ae25f
shuyan0723 <2737559680@qq.com>
2025-07-13 19:53:31 +0800

    ts类型

---
+++ TS/2.js
@@ -0,0 +1,4 @@
+var a = 1;
+// a='hello' // 不能将类型“string”分配给类型“number”。
+// let b:string = 'hello'
+a = 2;
---
+++ TS/3.ts
@@ -0,0 +1,9 @@
+// let a=1
+// a = 2;
+
+// let a:any=1
+// a='1'
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(1,2));
