049b63358e95fae6bb1e72714295ae8cbb551a1d
shuyan0723 <2737559680@qq.com>
2025-07-16 12:08:04 +0800

    单例+Fragment

--- js/singleton/readme.md
+++ js/singleton/readme.md
@@ -16,4 +16,15 @@ setItem(key.value) 和getItem(key)
 - static getInstance 方法 判断 instance并返回
      实例的时候一定要这样 
 - 性能特别好，好管理 
+# 单例模式
+
+- 实现一个登录弹窗
+     - 体验
+     - 不用跳转路由，盖在页面上 
+     Z-index display none|block
+     - 性能 
+     90% 用户 不登录
+     Model html css js 比较多 
+     推迟到第一次用的时候，单例
+     复用
 
---
+++ react/fragment/fragment-demo/src/App.jsx
@@ -0,0 +1,47 @@
+import { 
+  useState,
+  Fragment   // 文档碎片
+} from 'react'
+import './App.css'
+
+// function Demo(){
+//   return (
+//     // DOM 树多了一层不需要的节点，DOM解析性能下降，多迭代一层
+//     <Fragment>
+//       <h1>Hello</h1>
+//       <p>你好</p>
+//     </Fragment>
+//   )
+// }
+function Demo({items}){
+ return items.map(items =>(
+        <Fragment key={items.id}>
+          <h1>{items.name}</h1>
+          <p>{items.content}</p>
+        </Fragment>
+      ))
+   }
+  
+   
+
+function App() {
+     const items=[
+      {
+        id:1,
+        name:'钟文城',
+        content:'内容1'
+      },
+      {
+        id:2,
+        name:'赖泽阳',
+        content:'内容2'
+      }
+     ]
+  return (
+    <>
+      <Demo items={items}/>
+    </>
+  )
+}
+
+export default App
