c2e03ee166897c43ba1a3fc58b461b2ab0cd7558
shuyan0723 <2737559680@qq.com>
2025-08-12 17:04:04 +0800

    tts语音

--- react/transform-tts/README.md
+++ react/transform-tts/README.md
@@ -1,17 +1,28 @@
 # React Transformer TextToSpeech
 
-- transformer 文本转语音
-     transformer.js JS AI 机器学习库 
-     来自于huggingface 全球最大开源大模型社区
-     将模型下载到浏览器端，JS 开发者的智能战场未来
+- transformer
+    transformer.js JS AI 机器学习库
+    来自于huggingface 全球最大开源大模型社区
+    将模型下载到浏览器端，JS 开发者的智能战场未来
+    
+- 项目的亮点
+    - 使用transformer.js 的 端模型
+    - 使用Tailwindcss 原子css 几乎不需要写样式了
+        类名文档语义很好，特别适合 AI生成
+        高效解决适配 w-full + max-w-xl
+    - webworker nlp 任务
+        1. 延迟加载大模型
+        2. 在现实组件后再实例化worker 性能好
+        3. 卸载时移除事件 
+    - 封装组件
+        
+- 项目的难点
+    - 单例模式封装 MyTextToSpeechPipeline
+    - getInstance 只实例化一次
+    - 懒执行 
+    - promise.all + nlp 流程的理解 (tokenizer,model,vocoder)
 
-     - 项目的亮点
-          - 使用transformer.js 的 端模型
-          - 使用tailwindcss 原子css 几乎不需要写样式了
-              类名文档语义很好，特别适合AI 生成
-         - webworker nlp 任务
-              1. 延迟加载大模型
-              2. 在现实组件后再实例化 性能好
-              3. 卸载时移除事件监听
-         - 封装组件 
-     - 项目的难点
+audio 标签的url <- Object
+
+Object.createObjectURL 是一个用于创建一个唯一 URL，
+该 URL 可以引用由浏览器内部生成的对象（如 Blob 或 File 对象）的方法，常用于在网页中显示本地文件内容，如图片预览。
--- react/transform-tts/src/App.jsx
+++ react/transform-tts/src/App.jsx
@@ -5,24 +5,25 @@ import {
 } from 'react'
 import Progess from './components/Progress';
 import AudioPlayer from './components/AudioPlayer';
+
 import {
-  SPEAKERS,
-  DEFAULT_SPEAKER,
-}
-from './constants'
+  SPEAKERS, 
+  DEFAULT_SPEAKER
+} from './constants'
+
 function App() {
   // 界面状态
   // llm ready 大模型准备好了不？
-  const [ready,setReady]=useState(null);
+  const [ready, setReady] = useState(null);
   // 按钮点击 防止多次点击
-  const [disabled,setDisabled]=useState(false);
+  const [disabled, setDisabled] = useState(false);
   // 进度条数组
-  const [progressItem,setProgressItem]=useState([]);
- // 表单文本
-  const [text,setText]=useState('I love Hugging Face');
+  const [progressItems, setProgressItems] = useState([]);
+  // 表单文本
+  const [text, setText] = useState('I love Hugging Face!');
   // 音色
-  const [selectedSpeaker,setSelectedSpeaker]=useState('DEFAULT_SPEAKER');
-  const [output,setOutput]=useState(null);
+  const [selectedSpeaker, setSelectedSpeaker] = useState(DEFAULT_SPEAKER);
+  const [output, setOutput] = useState(null);
 
   const worker = useRef(null);
   useEffect(() => {
@@ -31,12 +32,33 @@ function App() {
     worker.current = new Worker(new URL('./worker.js', import.meta.url), {
       type: 'module'
     })
-    worker.current.postMessage({
-      text: '灵不灵，奔驰s680'
-    })
 
-    const onMessageReceived = () => {
+    const onMessageReceived = (e) => {
+      console.log(e, '来自主线程');
+      switch(e.data.status) {
+        case 'initiate':
+          // llm ready 了吗？
+          setReady(false);
+          setProgressItems(prev => [...prev, e.data])
+        break;
+        case 'download':
+        break;
+        case 'progress':
+        break;
+        case 'done':
+        break;
+        case 'complete':
+          setDisabled(false);
 
+        setProgressItems(
+          prev => prev.map(item => item.file!= e.data.file? item : {
+            ...item,
+            Progress: e.data.Progress
+          })
+        )
+        setOutput(e.data.output)
+        
+      }
     }
     worker.current.onmessage = onMessageReceived;
 
@@ -44,12 +66,112 @@ function App() {
       onMessageReceived
     )
   }, [])
+
+  const handleGenerateSpeech = () => {
+    setDisabled(true);
+    worker.current.postMessage({
+      text,
+      speaker_id: selectedSpeaker
+    });
+  }
+
+  const isLoading = ready === false;
+
   return (
-    <div className="flex">
-      <AudioPlayer 
-        audioUrl="https://cdn.freesound.org/previews/819/819183_12880153-lq.mp3"
-        mimeType="audio/mpeg"
-      />
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      {/* llm 初始化 */}
+      <div 
+      className="absolute z-50 top-0 left-0 w-full h-full transition-all 
+      px-8 flex flex-col justify-center text-center"
+      style={{
+        opacity: isLoading? 1 : 0,
+        pointerEvents: isLoading? 'all': 'none',
+        background: 'rgba(0,0,0,0.9)',
+        backdropFilter: 'blur(8px)'
+      }}
+      >
+         {
+          isLoading&&(
+            <label className='text-white text-xl p-3'>
+              Loading...
+              </label>
+
+          )
+         }
+         {
+          progressItems.map(data=>(
+            <div key={`${data.name}/${data.file}`}>
+            <Progess 
+            text={`${data.name}/${data.file}`}
+             percentage={data.Progress}
+
+            />
+            </div>
+          ))
+         }
+      </div>
+      {/* tts 功能区 */}
+      <div className="bg-white p-8 rounded-lg w-full max-w-xl m-2">
+        <h1 className="text-3xl font-semibold text-gray-800 mb-1 text-center">
+          In browser Text To Speech(端模型)
+        </h1>
+        <h2 className="text-base font-medium text-gray-700 mb-2 text-center">
+          Made with <a> Transfromer.js</a>
+        </h2>
+        <div className="mb-4">
+          <label htmlFor="text" className="block text-sm font-medium text-gray-600">
+            Text
+          </label>
+          <textarea id="text"
+            className="border border-gray-300 rounded-md p-2 w-full"
+            rows="4"
+            placeholder='Enter text here'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          >
+          </textarea>
+        </div>
+        <div className="mb-4">
+          <label 
+            htmlFor="speaker" 
+            className="block text-sm font-medium text-gray-600"
+          >
+          </label>
+          <select 
+            id="speaker"
+            className="border border-gray-300 rounded-md p-2 w-full"
+            value={selectedSpeaker}
+            onChange={(e)=>setSelectedSpeaker(e.target.value)}
+          >
+          {
+            // 可迭代对象快速转换成数组 [[key:val],[key1:value1],[key2:value1]]
+            Object.entries(SPEAKERS).map(([key, value]) => (
+              <option key={key} value={value}>
+                {key}
+              </option>
+            ))
+          }
+          </select>
+        </div>
+        <div className="flex justify-center">
+          <button
+            className={`${disabled? 
+              'bg-gray-400 cursor-not-allowed':
+              'bg-blue-500 hover-bg-blue-600'
+            } text-white rouned-md py-2 px-4`}
+            onClick={handleGenerateSpeech}
+            disabled={disabled}
+          >
+            {disabled? 'Generating...': 'Generate'}
+          </button>
+        </div>
+        {
+          output && <AudioPlayer 
+            audioUrl={output}
+            mimeType={"audio/wav"}
+          />
+        }
+      </div>
     </div>
   )
 }
