f16daa45ef55775403973fcd737932367acc4296
shuyan0723 <2737559680@qq.com>
2025-08-18 17:54:44 +0800

    function_call LLM大模型

---
+++ interview/ai/function_call/Demo/index.mjs
@@ -0,0 +1,46 @@
+import OpenAI from 'openai'; // es6 module
+
+const client = new OpenAI({
+    apiKey: 'sk-h91KBBHUC30pbiviNhD9I5YnyC7gcdd4CzPCKJcMhnkm7IT9', // 302 平台的 API 密钥
+    baseURL: 'https://api.302.ai/v1', // 302 平台的 API 地址
+})
+
+async function main() {
+    const resp = await client.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: "user",
+                content: "今天抚州天气怎么样?"
+            }
+        ],
+        // LLM 使用的工具
+        tools: [
+            {
+                type: 'function',
+                function: {
+                    name: "getWeather",
+                    description: "获取某个城市的天气",
+                    parameters: {
+                        type: "object",
+                        properties: {
+                            city: {
+                                type: "string"
+                            }
+                        },
+                        required: ["city"]
+                    }
+                }
+            }
+        ]
+    });
+
+     const toolCall=resp.choices[0].message.tool_calls?.[0];
+     console.log('大模型调用toolCall:',toolCall);
+
+
+     
+
+}
+
+main();
